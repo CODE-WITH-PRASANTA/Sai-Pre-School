53828d4e8ecfc78d1f3f4ea02a77f7d498990d71
swarnaprava-06032005 <150228737+swarnaprava-06032005@users.noreply.github.com>
2026-03-06 10:01:39 +0530

    HomeSecondCompo, About completed and responsive check

--- Frontend/src/Pages/Home/Home.jsx
+++ Frontend/src/Pages/Home/Home.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
-import Testimonials from '../../Components/Testimonial/Testimonial'
-import HomeComponent from '../../Components/HomeComponent/HomeComponent'
-import HomeSecondCompo from '../../Components/HomeSecondCompo/HomeSecondCompo'
+import Testimonials from '../../Components/Testimonial/Testimonial';
+import HomeComponent from '../../Components/HomeComponent/HomeComponent';
+import HomeSecondCompo from '../../Components/HomeSecondCompo/HomeSecondCompo';
+import About from '../../Components/About/About';
 
 const Home = () => {
   return (
     <>
       <HomeComponent/>
       <HomeSecondCompo/>
+      <About/>
       <Testimonials/>
     </>
   )
