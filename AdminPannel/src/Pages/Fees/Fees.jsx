1a108ef07bdfd20160ddcee5a6042e5f0d153cba
Ankitanayak28 <153258452+Ankitanayak28@users.noreply.github.com>
2026-03-09 14:35:47 +0530

    push

--- AdminPannel/src/Pages/Fees/Fees.jsx
+++ AdminPannel/src/Pages/Fees/Fees.jsx
@@ -1,14 +1,13 @@
 import React from 'react'
 import CollectFees from '../../Component/CollectFees/CollectFees'
-import StudentList from '../../Component/StudentList/StudentList'
-import FeesDetails from '../../Component/FeesDetails/FeesDetails'
+
 
 const Fees = () => {
   return (
     <div>
         <CollectFees/>
-        <StudentList/>
-        <FeesDetails/>
+       
+        
     </div>
   )
 }
