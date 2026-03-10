bb88535f3748d7c5095309d64d470e08fbd62d13
swarnaprava-06032005 <150228737+swarnaprava-06032005@users.noreply.github.com>
2026-03-10 17:59:03 +0530

    Contact Backend Done

--- Backend/server.js
+++ Backend/server.js
@@ -3,12 +3,13 @@ const cors = require("cors");
 const dotenv = require("dotenv");
 
 const connectDB = require("./config/db");
+
 const eventRoutes = require("./routes/eventRoutes");
 const newsRoutes = require("./routes/newsRoutes");
-// const eventRoutes = require("./routes/eventRoutes");
-// const testimonialRoutes = require("./routes/testimonialRoutes");
+const contactRoutes = require("./routes/contactRoutes");
 
 dotenv.config();
+
 connectDB();
 
 const app = express();
@@ -18,13 +19,15 @@ app.use(express.json());
 
 app.use("/uploads", express.static("uploads"));
 
+
+// API Routes
 app.use("/api", newsRoutes);
 app.use("/api", eventRoutes);
-// app.use("/api/events", eventRoutes);
-// app.use("/api/testimonials", testimonialRoutes);
+app.use("/api", contactRoutes);
+
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`🚀 Server running on port ${PORT}`);
 });
